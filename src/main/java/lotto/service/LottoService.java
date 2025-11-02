09258980e44557226cd9e13ac6c15f295a1cdf52
iamjunhyeong <dddodo447@gmail.com>
2025-11-03 02:22:10 +0900

    feat(service): 로또 서비스 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,29 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
+import lotto.domain.LottoResult;
+import lotto.domain.Rank;
+
+import java.util.List;
+
 public class LottoService {
+
+    public List<Lotto> buyLottos(int money) {
+        int ticketCount = money / 1000;
+        return java.util.stream.IntStream.range(0, ticketCount)
+                .mapToObj(i -> LottoGenerator.generateLotto())
+                .toList();
+    }
+
+    public LottoResult checkResult(List<Lotto> tickets, Lotto winningLotto, int bonus) {
+        LottoResult lottoResult = new LottoResult();
+        for (Lotto ticket : tickets) {
+            int matchCount = ticket.countMatchingNumbers(winningLotto);
+            boolean bonusMatch = ticket.containsNumber(bonus);
+            Rank rank = Rank.valueOf(matchCount, bonusMatch);
+            lottoResult.addRank(rank);
+        }
+        return lottoResult;
+    }
 }
