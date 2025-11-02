1f4133b5a5116fb649978deba9a5e063f26f8045
iamjunhyeong <dddodo447@gmail.com>
2025-11-03 00:44:44 +0900

    feat(main,controller, service, util): 스켈레톤 코드 작성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,25 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.service.LottoService;
+import lotto.util.InputView;
+
+import java.util.List;
+
+public class LottoController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final LottoService lottoService = new LottoService();
+
+    public void run() {
+//        int money = inputView.inputMoney();
+//        List<Lotto> tickets = lottoService.buyLottos(money);
+//        outputView.printLottos(tickets);
+//
+//        Lotto winningLotto = inputView.inputWinningNumbers();
+//        int bonus = inputView.inputBonusNumber(winningLotto);
+//
+//        LottoResult result = lottoService.checkResult(tickets, winningLotto, bonus);
+//        outputView.printStatistics(result);
+    }
+}
